e36d2c66814fc77873431597b96e36fa656c5c82
Esteban2005-blip <ej.paredesma@uea.edu.ec>
2025-06-30 21:08:29 +0700

    Add files via upload

---
+++ SEMANA6.1.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+        lista.Agregar(10);
+        lista.Agregar(20);
+        lista.Agregar(30);
+
+        Console.WriteLine("Número de elementos: " + lista.ContarElementos());
+    }
+}
+
+
+
+
+
